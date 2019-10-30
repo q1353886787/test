000e5a06cbb5f7f1d109fdd83381d9547a1822a4
Your Name <you@example.com>
2019-10-30 08:38:43 +0800

    add snack

--- snack/snack.cpp
+++ snack/snack.cpp
@@ -2,32 +2,34 @@
 //
 
 #include "stdafx.h"
-#include <window.h>
+#include <windows.h>
+//1.贪吃蛇上下移动
+//2.贪吃蛇吃食物长大
+//3.贪吃蛇撞墙，吃到自己，gg
+//4.游戏结束，打印分数
 
 
 void init()
-{
-	
+{	
 }
 
 void gameloop()
 {
 	while(1)
 	{
-		sleep(100);
+		Sleep(100);//延时
 	}
 }
 
-void score
+void Score()
 {
-
 }
 
 int main(int argc, char* argv[])
 {
-	init();
-	score();
-	gameloop();
+	init();//画地图
+	Score();//游戏主循环
+	gameloop();//打印得分
 	return 0;
 }
 
