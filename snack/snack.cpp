158fdf6f07210b6edd1a329919397d7041818567
Your Name <you@example.com>
2019-10-30 08:32:26 +0800

    add snack

---
+++ snack/snack.cpp
@@ -0,0 +1,33 @@
+// snack.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <window.h>
+
+
+void init()
+{
+	
+}
+
+void gameloop()
+{
+	while(1)
+	{
+		sleep(100);
+	}
+}
+
+void score
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+	init();
+	score();
+	gameloop();
+	return 0;
+}
+
